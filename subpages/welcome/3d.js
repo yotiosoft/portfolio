8620893c1bbd228913f697fbd58260acb616e329
YotioSoft <ytani0323@gmail.com>
2024-06-06 23:42:29 +0900

    3d temp

--- subpages/welcome/3d.js
+++ subpages/welcome/3d.js
@@ -13,28 +13,28 @@ window.addEventListener('load', () => {
     renderer.setSize( width, height );
     renderer.setAnimationLoop( animate );
 
-    const geometry_yellow = new THREE.BoxGeometry(1, 2.5, 0.1);
+    const geometry_yellow = new THREE.BoxGeometry(1, 2.5, 0.25);
     const material_yellow = new THREE.MeshBasicMaterial({ color: 0xfff200 });
     const wall_yellow = new THREE.Mesh(geometry_yellow, material_yellow);
-    wall_yellow.position.set(-0.5, 0, 0);
+    wall_yellow.position.set(-0.75, 0, 0);
     scene.add(wall_yellow);
 
-    const geometry_green = new THREE.BoxGeometry(1, 2, 0.1);
+    const geometry_green = new THREE.BoxGeometry(1, 2, 0.5);
     const material_green = new THREE.MeshBasicMaterial({ color: 0x23b14b });
     const wall_green = new THREE.Mesh(geometry_green, material_green);
-    wall_green.position.set(-0.25, 0, 0.5);
+    wall_green.position.set(-0.25, -0.25, 0);
     scene.add(wall_green);
 
-    const geometry_red = new THREE.BoxGeometry(1, 1.5, 0.1);
+    const geometry_red = new THREE.BoxGeometry(1, 1.5, 0.75);
     const material_red = new THREE.MeshBasicMaterial({ color: 0xed2324 });
     const wall_red = new THREE.Mesh(geometry_red, material_red);
-    wall_red.position.set(0, 0, 1);
+    wall_red.position.set(0.25, -0.5, 0);
     scene.add(wall_red);
 
-    const geometry_blue = new THREE.BoxGeometry(1, 1, 0.1);
+    const geometry_blue = new THREE.BoxGeometry(1, 1, 1);
     const material_blue = new THREE.MeshBasicMaterial({ color: 0x00a2e8 });
     const wall_blue = new THREE.Mesh(geometry_blue, material_blue);
-    wall_blue.position.set(0.25, 0, 1.5);
+    wall_blue.position.set(0.75, -0.75, 0);
     scene.add(wall_blue);
 
     camera.position.z = 5;
